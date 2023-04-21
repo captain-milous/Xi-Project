9b99807b1e4dbb2d298e984af1ef19446a74109f
captain-milous <milda.tesar3@gmail.com>
2023-04-21 08:05:36 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+  
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Xi Project</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+</head>
+<body class="font-weight-bolder">
+<!-- Header -->
+  <div class="p-3 fixed-top text-bg-dark">
+    <div class="container">
+
+      <header class="d-flex flex-wrap justify-content-center ">
+        <a href="index.html" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+          <span class="fs-4 text-white">Obchůdek</span>
+        </a>
+        <ul class="nav nav-pills">
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-secondary" aria-current="page">Hlavní stránka</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-white">O nás</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Kontakt</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Přihlášení</a></li>
+        </ul>
+      </header>
+
+    </div>
+  </div>
+<br>
+<br>
+
+
+
+
+<!-- Footer -->
+<br>
+<br>
+<br>
+<br>
+
+  <div class="container-fluid p-2 bg-dark">
+    <footer class="my-4 align-items-center text-center">
+      <a href="https://github.com/captain-milous/Xi-Project" class="text-white em text-decoration-none">Copyright © Miloš Tesař C3b, 2023</a>
+    </footer>
+  </div>
+
+</body>
+</html>
