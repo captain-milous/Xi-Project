f5ec2d751c185c1eadb03b8031e8fdfbb9803baf
captain-milous <milda.tesar3@gmail.com>
2023-04-26 22:09:55 +0200

    Menu

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
         </a>
         <ul class="nav nav-pills">
           <li class="nav-item"><a href="index.php" class="nav-link px-2 text-secondary" aria-current="page">Hlavní stránka</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-white">O nás</a></li>
-          <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Kontakt</a></li>
+          <li class="nav-item"><a href="about-us.php" class="nav-link px-2 text-white">O nás</a></li>
+          <li class="nav-item"><a href="contact.php" class="nav-link px-2 text-white">Kontakt</a></li>
           <li class="nav-item"><a href="login.php" class="nav-link px-2 text-white">Přihlášení</a></li>
         </ul>
       </header>
