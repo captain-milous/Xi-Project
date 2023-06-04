f06aac38bf900bb1c88a5020201f76381e323d2e
captain-milous <milda.tesar3@gmail.com>
2023-06-04 09:59:48 +0200

    Header

--- header.php
+++ header.php
@@ -1,32 +1,46 @@
+<?php
+  if(isset($_GET["mssg"])){
+    $oo = $_GET["mssg"];
+    ?>
+        <script defer>
+            alert("<?php echo $oo;?>");
+        </script>
+    <?php
+}
+?>
+
 <!-- Header -->
 
 <div class="p-3 fixed-top text-bg-dark">
-    <div class="container">
+  <div class="container">
 
-      <header class="d-flex flex-wrap justify-content-center ">
-        <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-          <span class="fs-4 text-white"><img src="images/white-logo.png" class="logo">Milošův Bar</span>
-        </a>
-        <ul class="nav nav-pills">
-          <li class="nav-item"><a href="index.php" class="nav-link px-2 text-white">Hlavní stránka</a></li>
-          <li class="nav-item"><a href="drinks.php" class="nav-link px-2 text-white">Drink Menu</a></li>
-          <li class="nav-item"><a href="contact.php" class="nav-link px-2 text-white">Kontakt</a></li>
+    <header class="d-flex flex-wrap justify-content-center ">
+
+      <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <span class="fs-4 text-white"><img src="images/white-logo.png" class="logo"> Incredible Combo Twins</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="index.php" class="nav-link px-2 text-white">Hlavní stránka</a></li>
+        <li class="nav-item"><a href="drinks.php" class="nav-link px-2 text-white">Drink Menu</a></li>
+        <li class="nav-item"><a href="contact.php" class="nav-link px-2 text-white">Kontakt</a></li>
           <?php
             if(isset($_SESSION["id"])){
           ?>
-            <li class="nav-item" id="login"><a href="Scripts/logout.php" class="nav-link px-2 text-white">Odhlásit se</a></li>
+        <li class="nav-item" id="login"><a href="Scripts/logout.php" class="nav-link px-2 text-white">Odhlásit se</a></li>
           <?php
             } else {
           ?>
-            <li class="nav-item" id="login"><a href="login.php" class="nav-link px-2 text-white">Přihlásit se</a></li>
+        <li class="nav-item" id="login"><a href="login.php" class="nav-link px-2 text-white">Přihlásit se</a></li>
           <?php
             }
           ?>
-        </ul>
-      </header>
+      </ul>
 
-    </div>
+    </header>
+
   </div>
+</div>
 
 <br>
 <br>
