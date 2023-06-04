852358c8c3a3147bf295c8631913bcb140057cd9
captain-milous <milda.tesar3@gmail.com>
2023-06-04 12:56:15 +0200

    Reservation
    jen frontend

--- reservation.php
+++ reservation.php
@@ -31,12 +31,41 @@ if(!isset($_SESSION["id"])){
     <div class="container my-4">
       <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg bg-light">
         <div class="col-lg-12 p-3 p-lg-5 pt-lg-3">
+
           <h1 class="display-5 fw-bold lh-1">Udělej si u nás rezervaci!</h1>
-            <br>
-            <div>
-              
+          <br>
+          <form action="Scripts/reserve.php" method="post">
+
+            <div class="form-group">
+                <label for="name">Jméno</label>
+                <input type="text" class="form-control" id="name" placeholder="Zadejte své jméno">
             </div>
-            <br>
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="Zadejte svůj email">
+            </div>
+
+            <div class="form-group">
+                <label for="date">Datum rezervace</label>
+                <input type="date" class="form-control" id="date">
+            </div>
+
+            <div class="form-group">
+                <label for="time">Čas rezervace</label>
+                <input type="time" class="form-control" id="time">
+            </div>
+
+            <div class="form-group">
+                <label for="guests">Počet hostů</label>
+                <input type="number" class="form-control" id="guests">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Rezervovat</button>
+          </form>
+
+          <small>Zatím mimo provoz</small>
+
           </div>
         </div>
     </div>
